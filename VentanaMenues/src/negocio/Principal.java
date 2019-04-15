82a8ea5bcaa0c1fd8dac371c38232354a4cd1042
Lucastini <47428112+Lucastini@users.noreply.github.com>
2019-04-15 00:28:04 -0300

    Add files via upload

---
+++ VentanaMenues/src/negocio/Principal.java
@@ -0,0 +1,13 @@
+
+package negocio;
+
+import presentacion.VentanaMenu;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        VentanaMenu ventana=new VentanaMenu();
+        ventana.setVisible(true);
+    }
+    
+}
